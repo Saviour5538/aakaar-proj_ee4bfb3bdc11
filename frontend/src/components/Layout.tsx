14aaf1e0447ade0543f6c75bb00721648c881c90
Adarsh Kumar Singh <143704495+Saviour5538@users.noreply.github.com>
2026-07-06 00:32:51 +0530

    Aakaar: Add frontend/src/components/Layout.tsx

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Layout: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
